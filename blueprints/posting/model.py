1a47be356bb47274026d26025ba43fe7fd21bef4
Fadhil Abdulkarim <fadhil@alterra.id>
2020-02-06 10:17:22 +0700

    init on second level, db made

--- blueprints/posting/model.py
+++ blueprints/posting/model.py
@@ -50,3 +50,49 @@ class TopLevels(db.Model):
         return '<Top Level %r>' % self.title
 
 
+
+class SecondLevels(db.Model):
+    __tablename__ = "second_level"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    #content type -- either comment or answer
+    content_type = db.Column(db.String(30), nullable=False) 
+    html_content = db.Column(db.Text, nullable=False)
+    #no banner
+    #no views
+    # status should merged into one? 0 - normal, 2 - deleted? should c/a be deletable?
+    # deleted = db.Column(db.Boolean, nullable=False)
+    # closed = db.Column(db.Boolean, nullable=False)
+    content_status = db.Column(db.SmallInteger, nullable=False)
+    point = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    #editable?
+    updated_at = db.Column(db.DateTime, server_onupdate=db.func.now())
+
+
+
+    #sementara, html_content di output dalam bentuk string
+    response_fields = {
+        'id': fields.Integer,
+        'user_id': fields.Integer,
+        'title': fields.String,
+        'content_type': fields.String,
+        'html_content': fields.String,
+        'content_status': fields.Integer,
+        'point': fields.Integer,
+        'created_at': fields.String,
+        'updated_at': fields.String
+    }
+
+    def __init__(self, user_id, title, content_type, html_content):
+        self.user_id = user_id
+        self.title = title
+        self.content_type = content_type
+        self.html_content = html_content
+        self.content_status = 0
+        self.point = 0
+        #no kwargs for banner
+
+    def __repr__(self):
+        return '<Second Level %r>' % self.title
