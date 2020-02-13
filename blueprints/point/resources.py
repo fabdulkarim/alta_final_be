d8b8e98e3da6898ccac3591c2bb453f03bef75e1
Fadhil Abdulkarim <fadhil@alterra.id>
2020-02-13 22:20:14 +0700

    reworked GET on /point to return list of object stuffs

--- blueprints/point/resources.py
+++ blueprints/point/resources.py
@@ -30,16 +30,17 @@ class PointCRU(Resource):
         # args = parser.parse_args()
 
         # if args['content_type'] in ['article','question']:
-        # qry = Points.query.filter_by(locator_id=args['locator_id'])
+        qry = Points.query #.filter_by(locator_id=args['locator_id'])
         qry = qry.filter_by(user_id=user_id)
         # .filter_by(content_type=args['content_type']).first()
         rows = []
         for que in qry:
-            rows.append(marshal(que,Points.response_fields))
+            marshal_point = marshal(que, Points.response_fields)
+            rows.append(marshal_point)
 
         # if qry.deleted == False:
         # return all, if None returns empty?
-        return marshal(rows), 200, {'Content-Type':'application/json'}
+        return rows, 200, {'Content-Type':'application/json'}
         
         # return mar
         #yang direturn apa?
